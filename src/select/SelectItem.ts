e21da3c21aca554d60e1d79b25fa3d1e2d43d06d
anuraghazra <hazru.anurag@gmail.com>
2020-08-28 11:48:15 +0530

    feat(select): virtual focus on mouse move over items

--- src/select/SelectItem.ts
+++ src/select/SelectItem.ts
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   useCompositeItem,
   CompositeItemHTMLProps,
@@ -6,6 +7,7 @@ import {
 import { SELECT_KEYS } from "./__keys";
 import { SelectStateReturn } from "./useSelectState";
 import { createHook, createComponent } from "reakit-system";
+import { useLiveRef, hasFocusWithin } from "reakit-utils";
 
 export type SelectItemOptions = CompositeItemOptions &
   Pick<SelectStateReturn, "setSelected" | "selected"> & {
@@ -18,12 +20,31 @@ const useSelectItem = createHook<SelectItemOptions, SelectItemHTMLProp>({
   name: "selectItem",
   compose: useCompositeItem,
   keys: ["value", ...SELECT_KEYS],
-  useProps({ selected, value, setSelected }, { ref: htmlRef, ...htmlProps }) {
+  useProps(
+    { selected, value, setSelected, disabled },
+    { ref: htmlRef, onMouseMove: htmlOnMouseMove, ...htmlProps },
+  ) {
+    const onMouseMoveRef = useLiveRef(htmlOnMouseMove);
+
+    const onMouseMove = React.useCallback(
+      (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+        onMouseMoveRef.current?.(event);
+        if (event.defaultPrevented) return;
+        if (disabled) return;
+        if (hasFocusWithin(event.currentTarget)) return;
+
+        event.currentTarget.focus();
+      },
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      [disabled],
+    );
+
     return {
       role: "option",
       "aria-label": value,
       "data-selected": selected.includes(value),
       "data-value": value,
+      onMouseMove,
       onClick: e => {
         e.preventDefault();
         setSelected(value);
