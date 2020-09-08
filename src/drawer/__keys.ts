f25f99c695b12c048ad0a70150bf021e85276889
Anurag Hazra <hazru.anurag@gmail.com>
2020-09-08 18:33:28 +0530

    feat: added drawer component (#20)
    * feat: added drawer component
    
    * chore: review updates
    
    * chore: minor updates
    
    * chore: remove unused import

---
+++ src/drawer/__keys.ts
@@ -0,0 +1,30 @@
+const DRAWER_STATE_KEYS = [
+  "baseId",
+  "unstable_idCountRef",
+  "visible",
+  "animated",
+  "animating",
+  "setBaseId",
+  "show",
+  "hide",
+  "toggle",
+  "setVisible",
+  "setAnimated",
+  "stopAnimation",
+  "modal",
+  "unstable_disclosureRef",
+  "setModal",
+] as const;
+export const DRAWER_KEYS = [
+  ...DRAWER_STATE_KEYS,
+  "hideOnEsc",
+  "hideOnClickOutside",
+  "preventBodyScroll",
+  "unstable_initialFocusRef",
+  "unstable_finalFocusRef",
+  "unstable_orphan",
+  "unstable_autoFocusOnShow",
+  "unstable_autoFocusOnHide",
+] as const;
+export const DRAWER_BACKDROP_KEYS = DRAWER_STATE_KEYS;
+export const DRAWER_DISCLOSURE_KEYS = DRAWER_BACKDROP_KEYS;
---
+++ src/drawer/index.ts
@@ -0,0 +1,9 @@
+export * from "./Drawer";
+export * from "./DrawerCloseButton";
+export {
+  useDialogState as useDrawerState,
+  DialogBackdrop as DrawerBackdrop,
+  useDialogBackdrop as useDrawerBackdrop,
+  DialogDisclosure as DrawerDisclosure,
+  useDialogDisclosure as useDrawerDisclosure,
+} from "reakit/Dialog";
