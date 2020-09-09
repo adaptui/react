08a1064c4f93858eedb36dc0d84df16e4db15ad7
Anurag Hazra <hazru.anurag@gmail.com>
2020-09-09 17:25:40 +0530

    test: added tests for Slider (#27)
    * test: jest testing setup
    
    * test: fixed jest typescript definitions
    
    * fix: test script
    
    * chore(deps): move deps to dev
    
    * test: added tests for accordion
    
    * test: added husky precommit test
    
    * test: axe container test
    
    * chore(infra): added github action test
    
    * test: added tests for number input
    
    * fix: a11y aria-valuetext issue with NumberInput
    
    * test: added tests for Slider

---
+++ src/slider/__tests__/Slider.test.tsx
@@ -0,0 +1,170 @@
+import * as React from "react";
+import { axe } from "jest-axe";
+import { render, press } from "reakit-test-utils";
+
+import {
+  Slider,
+  SliderThumb,
+  SliderTrack,
+  SliderInput,
+  useSliderState,
+  SliderFilledTrack,
+  SliderStateProps,
+} from "..";
+import {
+  sliderHorizontalStyle,
+  sliderHorizontalTrackStyle,
+  sliderHorizontalFilledTractStyle,
+  sliderHorizontalThumbStyle,
+} from "../stories/styles";
+
+export const SliderComp = (props: SliderStateProps) => {
+  const slider = useSliderState(props);
+
+  return (
+    <Slider {...slider} data-testid="slider" style={sliderHorizontalStyle}>
+      <SliderTrack {...slider} style={sliderHorizontalTrackStyle}>
+        <SliderFilledTrack
+          {...slider}
+          style={sliderHorizontalFilledTractStyle}
+        />
+      </SliderTrack>
+      <SliderThumb
+        {...slider}
+        aria-label="slider-thumb"
+        data-testid="slider-thumb"
+        style={{
+          ...sliderHorizontalThumbStyle,
+          transform: slider.state.isDragging
+            ? "translateY(-50%) scale(1.15)"
+            : sliderHorizontalThumbStyle.transform,
+        }}
+      />
+      <SliderInput data-testid="slider-input" {...slider} name="slider" />
+    </Slider>
+  );
+};
+
+describe("Slider", () => {
+  it("should render correctly", () => {
+    const { getByTestId: testId } = render(<SliderComp />);
+
+    expect(testId("slider-thumb")).not.toHaveFocus();
+    press.Tab();
+    expect(testId("slider-thumb")).toHaveFocus();
+  });
+
+  it("should have proper keyboard navigation", () => {
+    const { getByTestId: testId } = render(<SliderComp min={0} max={100} />);
+
+    expect(testId("slider-thumb")).not.toHaveFocus();
+    press.Tab();
+    expect(testId("slider-thumb")).toHaveFocus();
+
+    press.ArrowRight();
+    press.ArrowRight();
+    press.ArrowRight();
+    expect(testId("slider-input")).toHaveValue("53");
+    press.ArrowLeft();
+    press.ArrowLeft();
+    press.ArrowLeft();
+    expect(testId("slider-input")).toHaveValue("50");
+    press.PageUp();
+    expect(testId("slider-input")).toHaveValue("60");
+    press.PageDown();
+    expect(testId("slider-input")).toHaveValue("50");
+    press.Home();
+    expect(testId("slider-input")).toHaveValue("0");
+    press.End();
+    expect(testId("slider-input")).toHaveValue("100");
+  });
+
+  it("should behave properly on reverse: true", () => {
+    const { getByTestId: testId } = render(
+      <SliderComp isReversed min={0} max={100} />,
+    );
+
+    expect(testId("slider-thumb")).not.toHaveFocus();
+    press.Tab();
+    expect(testId("slider-thumb")).toHaveFocus();
+
+    press.ArrowRight();
+    press.ArrowRight();
+    press.ArrowRight();
+    expect(testId("slider-input")).toHaveValue("47");
+    press.ArrowLeft();
+    press.ArrowLeft();
+    press.ArrowLeft();
+    expect(testId("slider-input")).toHaveValue("50");
+    press.PageUp();
+    expect(testId("slider-input")).toHaveValue("40");
+    press.PageDown();
+    expect(testId("slider-input")).toHaveValue("50");
+    press.Home();
+    expect(testId("slider-input")).toHaveValue("0");
+    press.End();
+    expect(testId("slider-input")).toHaveValue("100");
+  });
+
+  it("should behave proper min, max values", () => {
+    const { getByTestId: testId } = render(<SliderComp min={-50} max={50} />);
+
+    expect(testId("slider-thumb")).not.toHaveFocus();
+    press.Tab();
+    expect(testId("slider-thumb")).toHaveFocus();
+
+    // middle
+    expect(testId("slider-input")).toHaveValue("0");
+
+    press.Home();
+    expect(testId("slider-input")).toHaveValue("-50");
+    press.End();
+    expect(testId("slider-input")).toHaveValue("50");
+  });
+
+  it("should behave proper step", () => {
+    const { getByTestId: testId } = render(<SliderComp step={25} />);
+
+    expect(testId("slider-thumb")).not.toHaveFocus();
+    press.Tab();
+    expect(testId("slider-thumb")).toHaveFocus();
+
+    expect(testId("slider-input")).toHaveValue("50");
+
+    press.ArrowLeft();
+    expect(testId("slider-input")).toHaveValue("25");
+    press.ArrowRight();
+    expect(testId("slider-input")).toHaveValue("50");
+  });
+
+  it("should behave properly on disabled", () => {
+    const { getByTestId: testId } = render(<SliderComp isDisabled />);
+
+    expect(testId("slider-thumb")).not.toHaveFocus();
+    expect(testId("slider")).toHaveAttribute("aria-disabled");
+    press.Tab();
+    expect(testId("slider-thumb")).toHaveFocus();
+
+    press.ArrowLeft();
+    expect(testId("slider-input")).toHaveValue("50");
+  });
+
+  it("should behave properly on readonly", () => {
+    const { getByTestId: testId } = render(<SliderComp isReadOnly />);
+
+    expect(testId("slider-thumb")).not.toHaveFocus();
+    expect(testId("slider-thumb")).toHaveAttribute("aria-readonly");
+    press.Tab();
+    expect(testId("slider-thumb")).toHaveFocus();
+
+    press.ArrowLeft();
+    expect(testId("slider-input")).toHaveValue("50");
+  });
+
+  test("Slider renders with no a11y violations", async () => {
+    const { container } = render(<SliderComp />);
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations();
+  });
+});
