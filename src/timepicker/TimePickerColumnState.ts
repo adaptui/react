f1d544301a7f066c98e2bb8a00bad934aa8aa7a0
Navin <navin007.a@gmail.com>
2020-10-09 13:03:48 +0530

    refactor(time-picker): ♻️  new timepicker state with much lesser code

--- src/timepicker/TimePickerColumnState.ts
+++ src/timepicker/TimePickerColumnState.ts
@@ -1,23 +1,43 @@
 import { useCompositeState } from "reakit";
 import { useControllableState } from "@chakra-ui/hooks";
 
+type ColumnType = "hour" | "minute" | "meridian";
 interface TimePickerColumnInitialProps {
-  value?: number;
-  onChange?: (v: number) => void;
+  value?: Date;
+  defaultValue?: Date;
+  onChange?: (v: Date) => void;
   visible?: boolean;
+  type?: ColumnType;
 }
 
 export const useTimePickerColumnState = (
   props: TimePickerColumnInitialProps = {},
 ) => {
-  const { value, onChange, visible } = props;
+  const {
+    value: time,
+    defaultValue = new Date(),
+    onChange,
+    visible,
+    type = "hour",
+  } = props;
 
-  const [selected, setSelected] = useControllableState({ value, onChange });
+  const [date, setDate] = useControllableState({
+    value: time,
+    defaultValue,
+    onChange,
+  });
+
+  const selected = getSelectedValueFromDate(date, type);
+
   const composite = useCompositeState({
     loop: true,
     wrap: true,
     orientation: "vertical",
   });
+
+  const setSelected = (value: number) => {
+    setDate(getSelectedDateFromValue(value, date, type));
+  };
 
   return { selected, setSelected, visible, ...composite };
 };
@@ -25,3 +45,41 @@ export const useTimePickerColumnState = (
 export type TimePickerColumnStateReturn = ReturnType<
   typeof useTimePickerColumnState
 >;
+
+function getSelectedValueFromDate(date: Date, type: ColumnType) {
+  if (type === "minute") return date.getMinutes();
+  if (type === "meridian") return date.getHours() >= 12 ? 1 : 0;
+  return date.getHours() % 12 || 12;
+}
+
+function getSelectedDateFromValue(value: number, date: Date, type: ColumnType) {
+  if (type === "minute") {
+    return new Date(date.setMinutes(value));
+  }
+
+  if (type === "meridian") {
+    let hours = date.getHours() % 12;
+
+    if (value === 1) {
+      hours = hours + 12;
+    } else {
+      hours = hours % 12;
+    }
+
+    return new Date(date.setHours(hours));
+  }
+
+  if (date.getHours() >= 12) {
+    if (value !== 12) {
+      value = value + 12;
+    }
+  } else {
+    if (value !== 12) {
+      value = value % 12;
+    } else {
+      value = 0;
+    }
+  }
+
+  return new Date(date.setHours(value));
+}
