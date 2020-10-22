9071fc35bd0f8ca2e482b4deffa76e4ccaac9b30
Navin <navin007.a@gmail.com>
2020-10-22 19:29:32 +0530

    refactor(slider): ♻️  restore old slider to fix tests

--- src/slider/__keys.ts
+++ src/slider/__keys.ts
@@ -8,9 +8,10 @@ const SLIDER_STATE_KEYS = [
 ] as const;
 export const SLIDER_KEYS = SLIDER_STATE_KEYS;
 export const SLIDER_FILLED_TRACK_KEYS = SLIDER_KEYS;
+export const SLIDER_INPUT_KEYS = SLIDER_FILLED_TRACK_KEYS;
 export const SLIDER_THUMB_KEYS = [
-  ...SLIDER_FILLED_TRACK_KEYS,
+  ...SLIDER_INPUT_KEYS,
   "id",
   "getAriaValueText",
 ] as const;
-export const SLIDER_TRACK_KEYS = SLIDER_FILLED_TRACK_KEYS;
+export const SLIDER_TRACK_KEYS = [...SLIDER_INPUT_KEYS, "id"] as const;
