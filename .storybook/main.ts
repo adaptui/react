7cc6bd561f1d6b4d30ae8f96fff8430a1bdbf167
Navin Moorthy <navin007.a@gmail.com>
2020-10-29 18:11:41 +0530

    feat(storybook): 📝  add csb, copy & preview for accordion (#123)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,9 @@
 module.exports = {
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-essentials",
-    "@storybook/addon-storysource",
-    "@storybook/addon-links",
+    "storybook-addon-preview",
+    "@storybook/addon-controls",
     "@storybook/addon-a11y",
+    "@storybook/addon-essentials",
   ],
 };
