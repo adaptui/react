f779effa6f3e058348ec108005c566c2d780e07f
Navin <navin007.a@gmail.com>
2020-11-02 17:08:00 +0530

    build(automation): 👷  improve transpile js automated script

--- scripts/generate-js.js
+++ scripts/generate-js.js
@@ -1,13 +1,36 @@
 const fs = require("fs");
 const path = require("path");
-const transpileTs = require("./transpile-ts");
+const chalk = require("chalk");
 const globFs = require("glob-fs")();
+const transpileTs = require("./transpile-ts");
 
-const folders = ["accordion"];
 function recurse(file) {
   // `file.pattern` is an object with a `glob` (string) property
   file.recurse = file.pattern.glob.indexOf("**") !== -1;
   return file;
+}
+
+// Create a new folder at the specified path
+function createDirectory(path) {
+  console.log("%c path", "color: #cc0088", path);
+  try {
+    if (!fs.existsSync(path)) {
+      fs.mkdirSync(path);
+    }
+  } catch (err) {
+    console.error(`[createDirectory]: Failed to create director at ${path}`);
+  }
+}
+
+function createFile(filePath, fileContent = "") {
+  fs.writeFile(filePath, fileContent, error => {
+    if (error) {
+      console.log(`Failed to create ${filePath}`, chalk.red.bold("ERROR"));
+      throw error;
+    }
+
+    console.log(`Create ${filePath}`, chalk.green.bold("DONE"));
+  });
 }
 
 const generateJsFiles = filePath => {
@@ -16,17 +39,20 @@ const generateJsFiles = filePath => {
     { encoding: "utf8" },
   );
   const appTemplateJs = transpileTs(accordionComponent);
-
-  fs.writeFileSync(
-    path.join(
-      process.cwd(),
-      filePath.replace("stories", "stories\\__js").replace(".tsx", ".jsx"),
-    ),
-    appTemplateJs,
+  const templateFilePath = filePath
+    .replace("stories", "stories/__js")
+    .replace(".tsx", ".jsx");
+  const templateDir = path.join(
+    process.cwd(),
+    templateFilePath.substring(0, templateFilePath.lastIndexOf("/")),
   );
+
+  createDirectory(templateDir);
+
+  const templatePath = path.join(process.cwd(), templateFilePath);
+
+  createFile(templatePath, appTemplateJs);
 };
 
 const files = globFs.use(recurse).readdirSync("**/*.component.tsx", {});
 files.forEach(filePath => generateJsFiles(filePath));
-
-console.log(files);
