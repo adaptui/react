9df41e5676a71fe4eb95a65c306fd7b7ad32369c
Anurag <hazru.anurag@gmail.com>
2020-11-04 13:26:43 +0530

    chore: segment component types

--- src/segment/stories/Segment.component.tsx
+++ src/segment/stories/Segment.component.tsx
@@ -1,40 +1,12 @@
 import React from "react";
-import { DateTimeFormatOpts } from "renderless-components/utils/types";
-import { Segment, SegmentField, useSegmentState } from "renderless-components";
+import {
+  Segment,
+  SegmentField,
+  useSegmentState,
+  SegmentStateProps,
+} from "renderless-components";
 
-interface AppProps {
-  /**
-   * segment value
-   */
-  value?: Date;
-  /**
-   * default segment value
-   */
-  defaultValue?: Date;
-  /**
-   * Sets formmating of date based on Intl.DateFormatOptions
-   *
-   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat
-   *
-   * @example
-   * {
-   *   year: "numeric",
-   *   month: "2-digit",
-   *   day: "2-digit",
-   *   weekday: "long",
-   * }
-   *
-   */
-  formatOptions?: DateTimeFormatOpts;
-  /**
-   * placeholder date
-   */
-  placeholderDate?: Date;
-  /**
-   * callback to fire on value change
-   */
-  onChange?: (value: Date, ...args: any[]) => void;
-}
+interface AppProps extends SegmentStateProps {}
 
 export const App: React.FC<AppProps> = props => {
   const state = useSegmentState(props);
