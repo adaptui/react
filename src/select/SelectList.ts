ee55c2171793a8de3491404d69b0694d29a4ebd6
Anurag Hazra <hazru.anurag@gmail.com>
2020-12-07 15:39:36 +0530

    chore: updated component aria-label warning reference links (#182)
    * chore: updated component aria label warning docs links
    
    * chore: fix capitalization

--- src/select/SelectList.ts
+++ src/select/SelectList.ts
@@ -33,7 +33,7 @@ export const SelectList = createComponent({
     useWarning(
       !props["aria-label"] && !props["aria-labelledby"],
       "You should provide either `aria-label` or `aria-labelledby` props.",
-      "See https://reakit.io/docs/select",
+      "See https://www.w3.org/TR/wai-aria-1.1/#listbox",
     );
     return useCreateElement(type, props, children);
   },
