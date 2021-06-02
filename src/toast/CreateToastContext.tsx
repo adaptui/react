5328041561c949090226563f1b97abe98185b3c2
Anurag Hazra <hazru.anurag@gmail.com>
2021-06-02 17:26:16 +0530

    refactor: extract out createToastContext types (#204)

--- src/toast/CreateToastContext.tsx
+++ src/toast/CreateToastContext.tsx
@@ -1,82 +1,23 @@
 import * as React from "react";
 import { createContext } from "@chakra-ui/react-utils";
 
-import {
-  genId,
-  ActionType,
-  useToastState,
-  DefaultToast,
-  StateReturnType,
-} from "./index";
-
-export type DefaultToastOptions<T extends DefaultToast> = Omit<
-  T,
-  "id" | "visible" | "reverseOrder" | "createdAt"
->;
-
-export type DefaultToastProviderOptions<T extends DefaultToast> = Partial<
-  DefaultToastOptions<T>
->;
-
-export type ConfigurableToastOptions<T extends DefaultToast> = Omit<
-  T,
-  "visible"
->;
-
-export type ToastOptions<T extends DefaultToast> = Partial<
-  ConfigurableToastOptions<T>
->;
-
-export interface ToastStore<T extends DefaultToast>
-  extends StateReturnType<T> {}
-
-export interface CreateToast<T extends DefaultToast, Content> {
-  createToast: CreateToastHandler<T, Content>;
-}
-
-export type CreateToastHandler<T extends DefaultToast, Content> = (
-  content: Content,
-  options?: ToastOptions<T>,
-) => T;
-
-export type AddToast<T extends DefaultToast, Content> = (
-  content: Content,
-  options?: ToastOptions<T>,
-) => string;
-
-export type ShowToast<T extends DefaultToast, Content> = (
-  content: Content,
-  options?: ToastOptions<T>,
-) => string;
-
-export type UpdateToast<T extends DefaultToast> = (
-  toastId: string,
-  toast: Partial<T>,
-) => void;
-
-export type UpdateFieldToast<T extends DefaultToast> = (
-  field: keyof T,
-  fieldValue: any,
-  toast: Partial<T>,
-) => void;
-
-export type UpdateAllToast<T extends DefaultToast> = (
-  toast: Partial<T>,
-) => void;
-
-export type DismissToast = (toastId?: string) => void;
-
-export type RemoveToast = (toastId?: string) => void;
-
-export type ToastHandlers<T extends DefaultToast, Content> = {
-  addToast: AddToast<T, Content>;
-  showToast: ShowToast<T, Content>;
-  updateToast: UpdateToast<T>;
-  updateFieldToast: UpdateFieldToast<T>;
-  updateAllToast: UpdateAllToast<T>;
-  dismissToast: DismissToast;
-  removeToast: RemoveToast;
-};
+import { genId, ActionType, useToastState, DefaultToast } from "./index";
+import type {
+  AddToast,
+  ShowToast,
+  ToastStore,
+  CreateToast,
+  RemoveToast,
+  UpdateToast,
+  ToastOptions,
+  DismissToast,
+  ToastHandlers,
+  UpdateAllToast,
+  UpdateFieldToast,
+  DefaultToastOptions,
+  CreateToastStoreReturn,
+  DefaultToastProviderOptions,
+} from "./CreateToastContext.types";
 
 export function createToastStore<T extends DefaultToast, Content>(
   defaultOptions: DefaultToastOptions<T>,
@@ -242,10 +183,3 @@ export function createToastStore<T extends DefaultToast, Content>(
     useToastHandlers,
   ] as CreateToastStoreReturn<T, Content>;
 }
-
-export type CreateToastStoreReturn<T extends DefaultToast, Content> = [
-  React.FC<Partial<DefaultToastOptions<T>>>,
-  () => ToastStore<T>,
-  () => CreateToast<T, Content>,
-  () => ToastHandlers<T, Content>,
-];
