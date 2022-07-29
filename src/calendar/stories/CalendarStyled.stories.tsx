a322de06f817a7b20972f5bf7e339feb10251ecd
mcnaveen <8493007+mcnaveen@users.noreply.github.com>
2022-07-29 11:13:41 +0530

    style(CalendarStyled): 💄  convert CalendarStyled to vanilla CSS

--- src/calendar/stories/CalendarStyled.stories.tsx
+++ src/calendar/stories/CalendarStyled.stories.tsx
@@ -9,7 +9,7 @@ import jsUtils from "./templates/UtilsJsx";
 import tsUtils from "./templates/UtilsTsx";
 import { CalendarStyled } from "./CalendarStyled.component";
 
-import "./tailwind.css";
+import "./CalendarStyled.css";
 
 type Meta = ComponentMeta<typeof CalendarStyled>;
 // type Story = ComponentStoryObj<typeof CalendarStyled>;
@@ -21,12 +21,6 @@ export default {
     layout: "centered",
     preview: createPreviewTabs({ js, ts, jsUtils, tsUtils }),
   },
-  decorators: [
-    Story => {
-      document.body.id = "tailwind";
-      return <Story />;
-    },
-  ],
 } as Meta;
 
 export const Default = () => {
